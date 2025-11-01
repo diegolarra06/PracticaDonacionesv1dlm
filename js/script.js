925ad588150fa303f76c1b1a991443a08934e08b
diego <diego@larrazabal40>
2025-11-01 20:20:43 +0100

    terminado el enunciado del 1 al 3 cargar datos desde json

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ let botonFinal = document.getElementById("botonFinal");
 botonFinal.addEventListener("click", finalizarTramite);
 });
 }
+/* ENUNCIADO 1-3: Cargar JSON (AJAX con fetch)*/
+function cargarDatosDesdeJson() {
+return fetch("").then(function (respuesta) {
+return respuesta.json();
+})
+.then(function (datos) {
+listaOrganizaciones = (datos.organizaciones || []).map(function (org) {
+return normalizarOrganizacion(org);
+});
+})
+.catch(function () {
+listaOrganizaciones = [];
+});
+}
+
+function normalizarOrganizacion(org) {
+let esPersonas = (typeof org.acogida !== "undefined") || (typeof org.rangoEdad !== "undefined");
+return {
+    id: org.id,
+    nombre: org.nombre,
+    tipo: esPersonas ? "personas" : "animales",
+    acogida: esPersonas ? !!org.acogida : null,
+    rangoEdad: esPersonas ? (org.rangoEdad || null) : null,
+    multiraza: !esPersonas ? !!org.multiraza : null,
+    ambito: !esPersonas ? (org.ambito || null) : null
+    };
+}
